467ee766632e933ba3e634fbbf220c0ce3a3b8d3
Jiaxin Shan <seedjeffwan@gmail.com>
2021-07-18 17:26:13 -0700

    Suppot to analyze repos under an organization

--- go.mod
+++ go.mod
@@ -3,5 +3,6 @@ module github.com/Jeffwan/github-devstats
 go 1.14
 
 require (
-	github.com/google/go-github/v37 v37.0.0 // indirect
+	github.com/gocarina/gocsv v0.0.0-20210516172204-ca9e8a8ddea8
+	github.com/google/go-github/v37 v37.0.0
 )
--- main.go
+++ main.go
@@ -2,17 +2,78 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"os"
 
+	"github.com/gocarina/gocsv"
 	"github.com/google/go-github/v37/github"
 )
 
-// Fetch all the public organizations' membership of a user.
-//
-func fetchOrganizations(username string) ([]*github.Organization, error) {
+// Code Repo struct
+type Package struct {
+	Name             string `json:"name"`
+	WatcherCount     int    `json:"watchers"`
+	StarsCount       int    `json:"starts"`
+	ForksCount       int    `json:"forks"`
+	IssuesCount      int    `json:"issues"`
+	PullRequestCount int    `json:"prs"`
+	Fork             bool   `json:"fork"`
+	Language         string `json:"language"`
+	LastUpdatedBy    string `json:"last_update_time"`
+	RepoUrl          string `json:"url"`
+	Description      string `json:"description"`
+}
+
+func fetchRepoDetails(owner string) ([]*Package, error) {
 	client := github.NewClient(nil)
-	orgs, _, err := client.Organizations.List(context.Background(), username, nil)
-	return orgs, err
+	var githubRepos []*github.Repository
+	var repositories []*Package
+
+	listOps := &github.RepositoryListOptions{
+		Sort:        "updated",
+		ListOptions: github.ListOptions{PerPage: 100},
+	}
+
+	// TODO: Fetch all the repositories under the owner. handle pagination
+	//nextPageToken := ""
+	for {
+		repos, resp, err := client.Repositories.List(context.Background(), owner, listOps)
+		if err != nil {
+			return repositories, errors.New(fmt.Sprintf("Problem in getting repository information %v\n", err))
+		}
+
+		githubRepos = append(githubRepos, repos...)
+		if resp.NextPageToken == "" {
+			break
+		}
+	}
+
+	fmt.Println(fmt.Sprintf("Fetch %d repositories under organization %v: ", len(githubRepos), owner))
+	for _, repo := range githubRepos {
+		p := &Package{
+			Name:          *repo.Name,
+			RepoUrl:       *repo.HTMLURL,
+			WatcherCount:  *repo.WatchersCount,
+			StarsCount:    *repo.StargazersCount,
+			ForksCount:    *repo.ForksCount,
+			IssuesCount:   *repo.OpenIssuesCount,
+			Fork:          *repo.Fork,
+			LastUpdatedBy: repo.GetUpdatedAt().Format("02-01-2006"),
+		}
+
+		if repo.Description != nil {
+			p.Description = *repo.Description
+		}
+
+		if repo.Language != nil {
+			p.Language = *repo.Language
+		}
+
+		repositories = append(repositories, p)
+	}
+
+	return repositories, nil
 }
 
 func main() {
@@ -20,13 +81,37 @@ func main() {
 	fmt.Print("Enter GitHub username: ")
 	fmt.Scanf("%s", &username)
 
-	organizations, err := fetchOrganizations(username)
+	repositories, err := fetchRepoDetails(username)
 	if err != nil {
 		fmt.Printf("Error: %v\n", err)
 		return
 	}
 
-	for i, organization := range organizations {
-		fmt.Printf("%v. %v\n", i+1, organization.GetLogin())
+	err = writeToCsv(repositories)
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		return
 	}
+}
+
+func writeToCsv(repositories []*Package) error {
+	repositoriesFile, err := os.OpenFile("devstats.csv", os.O_RDWR|os.O_CREATE, os.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+	defer repositoriesFile.Close()
+
+	// Go to the start of the file
+	if _, err := repositoriesFile.Seek(0, 0); err != nil {
+		panic(err)
+	}
+
+	// Get all structs as CSV string
+	//csvContent, err := gocsv.MarshalString(&repositories)
+	err = gocsv.MarshalFile(&repositories, repositoriesFile) // Use this to save the CSV back to the file
+	if err != nil {
+		panic(err)
+	}
+
+	return nil
 }
